afb04470a9ad0c49abd41b4147a8ed424320d701
Andy Parinas <andy.parinas@gmail.com>
2019-07-01 22:28:55 +1000

    Implemented Concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+    include Placeholder
+
     validates_presence_of :title, :body, :main_image
 
     #Custom Scope
@@ -7,4 +9,16 @@ class Portfolio < ApplicationRecord
     end
 
     scope :ruby_on_rails_portfolios, -> {where(subtitle: "Ruby on Rails")}
+
+
+    #Seting Default Value
+    after_initialize :set_defaults 
+
+    def set_defaults
+        # self.main_image ||= "https://dummyimage.com/600x400/000/fff"
+        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+
+        # the "||" prevents the value set by user to be overriden by default
+        # if the "||" is remove, what ever the value set by users, it will still be the default
+    end
 end
