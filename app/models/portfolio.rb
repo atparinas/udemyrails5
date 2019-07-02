bec033fb4c734ee64f07f4e137c09cefdf62d714
Andy Parinas <andy.parinas@gmail.com>
2019-07-02 15:50:51 +1000

    Integrated Portfolio nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,6 +4,9 @@ class Portfolio < ApplicationRecord
     #Relationship
     has_many :technologies
 
+    accepts_nested_attributes_for :technologies, 
+                                    reject_if: lambda { |attrs| attrs['name'].blank? }
+
     validates_presence_of :title, :body, :main_image
 
     #Custom Scope
