c2c24b3d194db1e57fd441d6b1c520cc4a8ece62
Andy Parinas <andy.parinas@gmail.com>
2019-07-03 12:47:59 +1000

    implemented guest user

---
+++ app/controllers/concerns/current_user_concerns.rb
@@ -0,0 +1,13 @@
+module CurrentUserConcerns
+    extend ActiveSupport::Concern
+
+    def current_user
+        super || guest_user
+
+    end
+
+    def guest_user
+        OpenStruct.new(name: "Guest User")
+    end
+
+end
