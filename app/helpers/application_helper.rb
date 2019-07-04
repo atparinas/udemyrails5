5b8ebf40205ec87096d640bd409a410fa0b5e3c2
Andy Parinas <andy.parinas@gmail.com>
2019-07-04 21:53:27 +1000

    added content_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
         end 
     end
 
+
+    def source_helper(layout_name)
+        if session[:source]
+            greeting = "Thanks for visiting me on #{session[:source]}"
+            content_tag(:p, greeting, class: "source-greeting")
+        end
+    end
+
 end
