554fe3352d41fec66f00c7eb48bfb96f4934ba6e
Andy Parinas <andy.parinas@gmail.com>
2019-07-05 21:40:48 +1000

    Added the copyright as gem file

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'devise', '~> 4.6', '>= 4.6.2'
 gem 'bootstrap', '~> 4.3.1'
 
 gem 'jquery-rails'
+
+gem 'parinas_view_tool', git: 'https://github.com/atparinas/copyright_gem'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,21 @@ class ApplicationController < ActionController::Base
     include CurrentUserConcerns
     include SetTitle
     
-  
+    before_action :set_copyright
+
+    def set_copyright
+        @copyright = ParinasViewTool::Renderer.copyright 'Andy Parinas', 'All rights reserved'
+    end
 
 end
+
+# module AndyViewTool
+
+#     class Renderer
+
+#         def self.copyright name, msg
+#             "&copy; #{Time.now.year} | <b> #{name} </b> #{msg}".html_safe
+#         end
+#     end
+
+# end
